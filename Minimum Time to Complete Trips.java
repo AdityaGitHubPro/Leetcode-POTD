76ee9d18fe0549fa56ad6eb1d7422b42dbb8d41e
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-03-07 10:00:50 +0530

    2187

--- Minimum Time to Complete Trips.java
+++ Minimum Time to Complete Trips.java
@@ -2,23 +2,62 @@ Question Link: https://leetcode.com/problems/minimum-time-to-complete-trips/
 
 
 
+// class Solution {
+//     public long minimumTime(int[] time, int totalTrips) {
+//        int n = time.length;
+//         long left = 1, right = (long)1e18;
+//         while (left < right) {
+//             long mid = (left + right) / 2;
+//             long cnt = 0;
+//             for (int t : time) {
+//                 cnt += mid / t;
+//                 if (cnt >= totalTrips) break;
+//             }
+//             if (cnt >= totalTrips) {
+//                 right = mid;
+//             } else {
+//                 left = mid + 1;
+//             }
+//         }
+//         return (int)left; 
+//     }
+// }
+
+
 class Solution {
-    public long minimumTime(int[] time, int totalTrips) {
-       int n = time.length;
-        long left = 1, right = (long)1e18;
-        while (left < right) {
+    int[] times;
+    int totalTrips;
+    
+    public long minimumTime(int[] times, int totalTrips) {
+        this.times = times;
+        this.totalTrips = totalTrips;
+        long left = 1;
+        long right = 0;
+        for (int time : times) {
+            right = Math.max(right, time);
+        }
+        right = right * totalTrips;
+        long ans = right;
+        while (left <= right) {
             long mid = (left + right) / 2;
-            long cnt = 0;
-            for (int t : time) {
-                cnt += mid / t;
-                if (cnt >= totalTrips) break;
-            }
-            if (cnt >= totalTrips) {
-                right = mid;
+            if (check(mid)) {
+                ans = mid;
+                right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return (int)left; 
+        return ans;
+    }
+    
+    public boolean check(long givenTime) {
+        long actualTrips = 0;
+        for (int time : times) {
+            actualTrips += givenTime / time;
+            if (actualTrips >= totalTrips) {
+                return true;
+            }
+        }
+        return actualTrips >= totalTrips;
     }
 }
