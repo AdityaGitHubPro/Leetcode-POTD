8c0dc1276b4e35a39934d8deccdb303f4a405b90
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-03-09 12:06:01 +0530

    142

---
+++ Linked List Cycle II.java
@@ -0,0 +1,27 @@
+Question Link: https://leetcode.com/problems/linked-list-cycle-ii/
+
+
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                ListNode ptr1 = head;
+                ListNode ptr2 = slow;
+                while (ptr1 != ptr2) {
+                    ptr1 = ptr1.next;
+                    ptr2 = ptr2.next;
+                }
+                return ptr1;
+            }
+        }
+        return null;
+    }
+}
