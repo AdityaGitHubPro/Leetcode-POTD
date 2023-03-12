b7c053f5c7bac1f50175a9abdd5a0cb2cbb45a48
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-03-12 11:02:58 +0530

    23

---
+++ Merge k Sorted Lists.java
@@ -0,0 +1,34 @@
+Question Link: https://leetcode.com/problems/merge-k-sorted-lists/
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> queue = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (ListNode list : lists) {
+            if (list != null) {
+                queue.add(list);
+            }
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (!queue.isEmpty()) {
+            tail.next = queue.poll();
+            tail = tail.next;
+            if (tail.next != null) {
+                queue.add(tail.next);
+            }
+        }
+        return dummy.next;
+    }
+}
