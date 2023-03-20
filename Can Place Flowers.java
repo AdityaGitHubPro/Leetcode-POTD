2bb26912495b5a974f56b87496c7b0db61e23f96
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-03-20 10:55:50 +0530

    605

---
+++ Can Place Flowers.java
@@ -0,0 +1,19 @@
+Question Link: https://leetcode.com/problems/can-place-flowers/
+
+
+
+class Solution {
+		public boolean canPlaceFlowers(int[] flowerbed, int n) {
+		for (var i = 0; i < flowerbed.length && n > 0; i++) 
+		if (flowerbed[i] == 0) {
+			var leftEmpty = (i == 0) || (flowerbed[i - 1] == 0);
+			var rightEmpty = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+			
+			if (leftEmpty && rightEmpty) {
+				flowerbed[i] = 1; // plant the flower
+				n--;
+			}
+		}
+		return n == 0;
+	}
+}
