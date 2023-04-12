3fb58c2aa5739f221e811d88d3bd653f09188ee4
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-04-12 10:11:27 +0530

    71

---
+++ Simplify Path.java
@@ -0,0 +1,21 @@
+Question Link: https://leetcode.com/problems/simplify-path/description/
+
+
+
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> dir_stack = new ArrayDeque<>();
+        for (String dir : path.split("/")) {
+            if (!dir_stack.isEmpty() && dir.equals("..")) {
+                dir_stack.removeLast();
+            } else if (!dir.equals(".") && !dir.equals("") && !dir.equals("..")) {
+                dir_stack.addLast(dir);
+            }
+        }
+        StringBuilder simplified_path = new StringBuilder();
+        for (String dir : dir_stack) {
+            simplified_path.append("/").append(dir);
+        }
+        return simplified_path.length() == 0 ? "/" : simplified_path.toString();
+    }
+}
