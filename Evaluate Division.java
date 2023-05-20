ee8c4e9da14066eeeb5abd702ebf8143e207fcb5
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-05-20 08:06:12 +0530

    399

---
+++ Evaluate Division.java
@@ -0,0 +1,43 @@
+class Solution {
+  Map<String, Map<String, Double>> map = new HashMap<>();
+  Set<String> seen = new HashSet<>();
+
+  private double dfs(String s, String target, double num) {
+    if (!map.containsKey(s)) return -1.0;
+    if (s.equals(target)) return num;
+    seen.add(s);
+
+    if (map.get(s).containsKey(target))
+      return num * map.get(s).get(target);
+
+    for (var div : map.get(s).entrySet()) {
+      if (!seen.contains(div.getKey())) {
+        var ans = dfs(div.getKey(), target, div.getValue());
+
+        if (ans != -1) return num * ans;
+      }
+    }
+    return -1.0;
+  }
+
+  public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+    var n = queries.size();
+    var ans = new double[n];
+
+    for (var i=0; i < equations.size(); i++) {
+      if (!map.containsKey(equations.get(i).get(0)))
+        map.put(equations.get(i).get(0), new HashMap<>());
+
+      if (!map.containsKey(equations.get(i).get(1)))
+        map.put(equations.get(i).get(1), new HashMap<>());
+
+      map.get(equations.get(i).get(0)).put(equations.get(i).get(1), values[i]);
+      map.get(equations.get(i).get(1)).put(equations.get(i).get(0), 1 / values[i]);
+    }
+    for (var i=0; i<n; i++) {
+      seen.clear();
+      ans[i] = dfs(queries.get(i).get(0), queries.get(i).get(1), 1.0);
+    }
+    return ans;
+  }
+}
